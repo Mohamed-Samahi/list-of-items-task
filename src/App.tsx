013222ee6f60d312a510ef2cc0a8dd7cbaa6041f
Mohamed-Samahi <mohamedsamahi2021@gmail.com>
2025-03-05 00:22:43 +0400

    Initial Project Setup, Typescript and Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div className='flex items-center justify-center w-screen h-screen text-5xl font-bold text-white bg-black'>
+      List of Items Task
+    </div>
+  );
+}
+
+export default App;
