8fcdf9757d1d7d7598e68c6c827a959b92471daa
Mohamed-Samahi <mohamedsamahi2021@gmail.com>
2025-03-05 17:48:41 +0400

    Wrapping App Component with Modal Provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { ModalProvider } from './context/ModalContext.tsx'
+import ModalWrapper from './components/modals/modal-wrapper.tsx'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ModalProvider>
+      <App />
+      <ModalWrapper />
+    </ModalProvider>
   </StrictMode>,
 )
