73ff9da1dd1772fed5012ed67ffe32c7a47c1fb5
Mohamed-Samahi <mohamedsamahi2021@gmail.com>
2025-03-05 22:44:31 +0400

    Fetch List Items, Mapping Over Fetched Items

--- src/components/items-list.tsx
+++ src/components/items-list.tsx
@@ -1,19 +1,40 @@
-import ListItem from "./list-item"
+import React, { useEffect } from 'react';
 
-const ItemsList = () => {
+import { useAppDispatch, useAppSelector } from '../redux/store';
+import { fetchItemsAction } from '../redux/actions/itemsActions';
+
+import ListItem from './list-item';
+
+import { ListItem as ListItemType } from '../types';
+
+const ItemsList: React.FC = () => {
+    const dispatch = useAppDispatch();
+    const { items, loading } = useAppSelector(state => state.items);
+
+    useEffect(() => {
+        dispatch(fetchItemsAction());
+    }, [dispatch]);
+
+    if (loading.fetching) return <div className='text-lg font-semibold text-Brand-700'>Loading items...</div>;
+
     return (
         <div className="flex flex-col items-start w-full h-full gap-4 overflow-y-auto">
-            <ListItem title="Title of the Item" description="This is the description of an item in the list" id={"10"} />
-            <ListItem title="Title of the Item" description="This is the description of an item in the list" id={"10"} />
-            <ListItem title="Title of the Item" description="This is the description of an item in the list" id={"10"} />
-            <ListItem title="Title of the Item" description="This is the description of an item in the list" id={"10"} />
-            <ListItem title="Title of the Item" description="This is the description of an item in the list" id={"10"} />
-            <ListItem title="Title of the Item" description="This is the description of an item in the list" id={"10"} />
-            <ListItem title="Title of the Item" description="This is the description of an item in the list" id={"10"} />
-            <ListItem title="Title of the Item" description="This is the description of an item in the list" id={"10"} />
-            <ListItem title="Title of the Item" description="This is the description of an item in the list" id={"10"} />
+            {items?.length === 0 ? (
+                <div className="w-full text-lg text-left text-Gray-600">
+                    No items found.
+                </div>
+            ) : (
+                items?.map((item: ListItemType) => (
+                    <ListItem
+                        key={item.id}
+                        id={item.id!}
+                        title={item.title}
+                        description={item.body}
+                    />
+                ))
+            )}
         </div>
-    )
-}
+    );
+};
 
-export default ItemsList
+export default ItemsList;
